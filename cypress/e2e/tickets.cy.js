8c51080573a9e5838c0ccce2f6b66061148d79c9
Natalia Kobeleva <cobeleva.natasha@gmail.com>
2024-04-02 12:15:29 +0400

    Тест на вход в админку

---
+++ cypress/e2e/tickets.cy.js
@@ -0,0 +1,28 @@
+import logins from "../fixtures/login";
+
+it("Main page is open", () => {
+  cy.visit("/");
+  cy.get(".page-nav__day").should("have.length", 7);
+  cy.get(".page-nav__day:nth-of-type(4)").click();
+});
+
+describe("Login to admin", () => {
+  beforeEach(() => {
+    cy.visit("https://qamid.tmweb.ru/admin/");
+  });
+
+  logins.forEach((login) => {
+    it(login.name, () => {
+      login.data.forEach((userData) => {
+        cy.get('[for="email"] > .login__input').type(userData.email);
+        cy.get('[for="pwd"] > .login__input').type(userData.password);
+        cy.contains("Авторизоваться").click();
+        if (login.name === "Successful login") {
+          cy.contains("Управление залами").should("be.visible");
+        } else {
+          cy.contains("Ошибка авторизации!").should("be.visible");
+        }
+      });
+    });
+  });
+});
